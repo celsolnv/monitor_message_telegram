0e2c83dea2b5fb438f2c8313865f52036e6a8783
Celso Vasconcelos <contato.celso.vasconcelos@gmail.com>
2024-07-25 01:27:14 -0300

    feat: change stategie

--- src/monitor/main.py
+++ src/monitor/main.py
@@ -12,7 +12,7 @@ CHAT_ID = chat_codes_aviator["1win_vip"]
 telegramClient = TelegramClient()
 app = telegramClient.get_client()
 
-HAS_REQUEST = False
+HAS_REQUEST = True
 
 MessageType = Literal["attention", "opportunity", "gale", "green", "red", "is_unknown"]
 last_type_message: MessageType = "is_unknown"
@@ -20,18 +20,19 @@ penultimate_type_message: MessageType = "is_unknown"
 
 
 def bet_aviator(reference: str):
-    data = {"reference_result": reference, "limit": "2"}
-    res = requests.post("http://localhost:8080/bet", data=data)
+    data = {"reference_result": f'{reference}x', "limit": "2"}
+    res = requests.post("http://localhost:8000/bet", json=data)
     print(res.json())
 
 
 # Função para monitor o grupo
 @app.on_message(filters.chat(CHAT_ID) & filters.text)
 def monitor_messages(_, message):
+    global last_type_message, penultimate_type_message
     message_text = message.text
     message_type = get_type_message(message_text)
-    print(f"Nova mensagem no grupo: {message_text}")
-    print(f"Mapeamento: {message_type}")
+    # print(f"Nova mensagem no grupo: {message_text}")
+    # print(f"Mapeamento: {message_type}")
 
     if (
         HAS_REQUEST
@@ -39,16 +40,18 @@ def monitor_messages(_, message):
         and penultimate_type_message != "is_unknown"
     ):
         if (
-            last_type_message == "red"
-            and penultimate_type_message == "red"
+            # last_type_message == "red"
+            penultimate_type_message == "red"
             and message_type == "opportunity"
         ):
             reference = get_reference_result(message_text)
             playsound("assets/notification.wav")
             bet_aviator(reference)
-    last_type_message, penultimate_type_message = message_type, last_type_message
+    if message_type == 'red' or message_type == 'green':
+        last_type_message, penultimate_type_message = message_type, last_type_message
     print(f"Último tipo de mensagem: {last_type_message}")
     print(f"Penúltimo tipo de mensagem: {penultimate_type_message}")
+    print('\n')
 
 
 app.run()
